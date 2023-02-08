ee7589905cc21b418a6883dd74d1e8d6c8edd461
Aaron Laney <jaj.laney@gmail.com>
2023-02-08 16:50:42 -0600

    add types

--- backend/types.go
+++ backend/types.go
@@ -5,3 +5,39 @@ type Flower struct {
 	name           string
 	price_per_stem float32
 }
+
+type Arrangement struct {
+	id           int
+	name         string
+	vessel_type  string // const or enum?
+	vessel_count int
+	foam_count   int
+	card_holder  bool
+	venmo        bool
+	paypal       bool
+	done         bool
+	json         string
+}
+
+type Multiplier struct {
+	name  string
+	value float32
+}
+
+type HardGood struct {
+	id    int
+	name  string
+	price float32
+}
+
+type Arrangement_HardGood struct {
+	arrangement_id int
+	hard_goods_id  int
+}
+
+type Flower_Arrangement struct {
+	flower_id      int
+	arrangement_id int
+	count          int
+	category       string // const or enum?
+}
