87225c7de0183ae8127cc3de977c7bd098b9fffd
Aaron Laney <jaj.laney@gmail.com>
2023-02-08 15:10:19 -0600

    rename file

--- backend/flower.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type Flower struct {
-	id   int
-	name string
-}
---
+++ backend/types.go
@@ -0,0 +1,7 @@
+package main
+
+type Flower struct {
+	id             int
+	name           string
+	price_per_stem float32
+}
