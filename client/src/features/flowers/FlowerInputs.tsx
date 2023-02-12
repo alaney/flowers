0df341e51d7b91e46dd29067fa4dbdf38802b171
Aaron Laney <jaj.laney@gmail.com>
2023-02-12 12:29:20 -0600

    form sort of getting there

--- client/src/features/flowers/FlowerInputs.tsx
+++ client/src/features/flowers/FlowerInputs.tsx
@@ -1,4 +1,4 @@
-import { FormControl, Grid, IconButton, InputAdornment, TextField, Typography } from "@mui/material"
+import { Grid, IconButton, InputAdornment, TextField, Typography } from "@mui/material"
 import React, { useState } from "react"
 import { Flower } from "../../types/Types"
 import SaveIcon from "@mui/icons-material/Done"
@@ -6,40 +6,37 @@ import UndoIcon from "@mui/icons-material/Undo"
 import { useAppDispatch } from "../../app/hooks"
 import { updateFlowerAsync } from "./flowersSlice"
 import cloneDeep from "lodash.clonedeep"
+import { Controller, SubmitHandler, useForm } from "react-hook-form"
 
 interface FlowerInputsProps {
   flower: Flower
 }
 
+interface FlowerInput {
+  name: string
+  price: string
+  count: number
+}
+
 const FlowerInputs: React.FC<FlowerInputsProps> = ({ flower }) => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors, isDirty },
+  } = useForm<FlowerInput>({
+    defaultValues: {
+      name: flower.name,
+      price: flower.pricePerBundle.toString(),
+      count: flower.stemCount,
+    },
+  })
   const [status, setStatus] = useState("idle")
   const [hasUpdates, setHasUpdates] = useState(false)
-  const [name, setName] = useState(flower.name)
-  const [price, setPrice] = useState(flower.pricePerBundle.toString())
-  const [count, setCount] = useState(flower.stemCount)
   const dispatch = useAppDispatch()
 
-  const updateFlowerName = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-    setHasUpdates(true)
-    const v = e.target.value
-    setName(v)
-  }
-
-  const updateFlowerPrice = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-    setHasUpdates(true)
-    const v = e.target.value
-    setPrice(v)
-  }
-
-  const updateFlowerCount = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-    setHasUpdates(true)
-    const v = e.target.value
-    setCount(Number(v))
-  }
-
-  const onSave = async () => {
+  const onSave: SubmitHandler<FlowerInput> = async (flowerUpdate) => {
     setStatus("loading")
-    const p = await dispatch(updateFlowerAsync({ ...cloneDeep(flower), name: name }))
+    const p = await dispatch(updateFlowerAsync({ ...cloneDeep(flower), name: flowerUpdate.name, pricePerBundle: Number(flowerUpdate.price), stemCount: flowerUpdate.count }))
     if (p.meta.requestStatus === "rejected") {
       setStatus("failed")
     } else {
@@ -49,58 +46,63 @@ const FlowerInputs: React.FC<FlowerInputsProps> = ({ flower }) => {
   }
 
   const onUndo = () => {
-    setName(flower.name)
-    setPrice(flower.pricePerBundle.toString())
-    setCount(flower.stemCount)
     setHasUpdates(false)
     setStatus("idle")
   }
 
   return (
-    <Grid container item spacing={2} alignItems="center">
-      <Grid item sm={2} md={2}>
-        <TextField required onChange={updateFlowerName} size="small" value={name}></TextField>
-      </Grid>
-      <Grid item sm={2} md={2}>
-        <TextField
-          required
-          onChange={updateFlowerPrice}
-          size="small"
-          value={price}
-          InputProps={{
-            startAdornment: <InputAdornment position="start">$</InputAdornment>,
-          }}
-          inputProps={{
-            pattern: "^\\d+(\\.\\d+)?$",
-          }}
-        ></TextField>
-      </Grid>
-      <Grid item sm={2} md={2}>
-        <TextField required onChange={updateFlowerCount} size="small" type="number" value={count}></TextField>
-      </Grid>
-      <Grid item sm={2} md={2}>
-        <Typography>{`$ ${flower.pricePerStem}`}</Typography>
-      </Grid>
-      {status === "failed" && (
-        <Grid item sm={1} md={1}>
-          <Typography>Failed!</Typography>
+    <Grid item container sm={12} md={12}>
+      <form onSubmit={handleSubmit(onSave)} style={{ width: "100%" }}>
+        <Grid item container spacing={2} alignItems="center" sm={12} md={12}>
+          <Grid item sm={2} md={2}>
+            <Controller name="name" control={control} rules={{ required: true }} render={({ field }) => <TextField size="small" error={!!errors.name} helperText={errors.name && "Must provide a value"} {...field} />} />
+          </Grid>
+          <Grid item sm={2} md={2}>
+            <Controller
+              name="price"
+              control={control}
+              rules={{ required: true, pattern: /^\d+(\.\d+)?$/ }}
+              render={({ field }) => (
+                <TextField
+                  InputProps={{
+                    startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                  }}
+                  size="small"
+                  error={!!errors.name}
+                  helperText={errors.name && "Must provide a value"}
+                  {...field}
+                />
+              )}
+            />
+          </Grid>
+          <Grid item sm={2} md={2}>
+            <Controller name="count" control={control} rules={{ required: true, min: 1 }} render={({ field }) => <TextField size="small" error={!!errors.name} helperText={errors.name && "Must provide a value"} {...field} />} />
+          </Grid>
+          <Grid item sm={2} md={2}>
+            <Typography>{`$ ${flower.pricePerStem}`}</Typography>
+          </Grid>
+          {status === "failed" && (
+            <Grid item sm={1} md={1}>
+              <Typography>Failed!</Typography>
+            </Grid>
+          )}
+          {status === "loading" && (
+            <Grid item sm={1} md={1}>
+              <Typography>Saving...</Typography>
+            </Grid>
+          )}
+          {isDirty && (
+            <Grid item sm={1} md={1}>
+              <IconButton type="submit">
+                <SaveIcon color="success" />
+              </IconButton>
+              <IconButton onClick={onUndo}>
+                <UndoIcon />
+              </IconButton>
+            </Grid>
+          )}
         </Grid>
-      )}
-      {status === "loading" && (
-        <Grid item sm={1} md={1}>
-          <Typography>Saving...</Typography>
-        </Grid>
-      )}
-      {hasUpdates && status !== "loading" && (
-        <Grid item sm={1} md={1}>
-          <IconButton onClick={onSave}>
-            <SaveIcon color="success" />
-          </IconButton>
-          <IconButton onClick={onUndo}>
-            <UndoIcon />
-          </IconButton>
-        </Grid>
-      )}
+      </form>
     </Grid>
   )
 }
