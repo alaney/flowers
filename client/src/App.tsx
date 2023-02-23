73b92ed08ed3df02203164dc961d90628f7dc9ba
Aaron Laney <jaj.laney@gmail.com>
2023-02-23 16:36:37 -0600

    OMG it works

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,9 +12,7 @@ function useRouteMatch(patterns: readonly string[]) {
 
   for (let i = 0; i < patterns.length; i += 1) {
     const pattern = patterns[i];
-    // console.log(pattern);
     const possibleMatch = matchPath(pattern, pathname);
-    // console.log(possibleMatch);
     if (possibleMatch !== null) {
       return possibleMatch;
     }
